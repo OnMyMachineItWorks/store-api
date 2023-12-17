7e96a2613396479c083188af0f7940b6519eb9fc
LazyCoder <huntyprograms@gmail.com>
2023-12-17 22:30:30 +0530

    Added controllers

---
+++ Controllers/StoreController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StoreManager.Api.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class StoreController : ControllerBase
+    {
+        [HttpGet]
+        public string GetStores()
+        {
+            return "get all Test Store";
+        }
+
+        [HttpGet("{id}")]
+        public string GetStoreById(int id)
+        {
+            return $"Test Store {id}";
+        }
+
+        [HttpPost]
+        public string CreateStore()
+        {
+            return "new test store";
+        }
+
+        [HttpPut("{id}")]
+        public string UpdateStore(int id)
+        {
+            return $"update Store {id}";
+        }
+
+        [HttpDelete("{id}")]
+        public string DeleteStore(int id)
+        {
+            return $"Delete Store {id}";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,18 +2,26 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-//builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
-//// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//}
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
+
+// Middlewares 
+app.UseRouting();
+app.UseAuthorization();
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
